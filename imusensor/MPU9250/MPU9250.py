c159511d8825fddca7488fea73b771a22bfe5a93
ahtakoru <ahtakoru@gmail.com>
2021-04-12 04:03:10 -0400

    Update MPU9250.py

--- imusensor/MPU9250/MPU9250.py
+++ imusensor/MPU9250/MPU9250.py
@@ -536,6 +536,8 @@ class MPU9250:
 		buffer[0] = subaddress
 		with self.i2c_device as i2c:
 			i2c.write_then_readinto(buffer, buffer, out_end=1+count, in_start=1)
+		print('At read register')
+		print(buffer)
 		return buffer[1:1+count]
 
 	def __writeAK8963Register(self, subaddress, data):
